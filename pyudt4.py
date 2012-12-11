2febe54a57e150be37ccf757779ae3e8c669eca5
Zeek Oni <web@cjhanks.name>
2012-12-11 02:17:42 -0800

    Add a pyudt4 for python interface and add licensing.

---
+++ pyudt4.py
@@ -0,0 +1,10 @@
+""" @module pyudt4 
+"""
+import udt4 
+
+class UdtSocket(object):
+    """ @class UdtSocket
+    A 'pythonic-wrapper' for the UDT4 library
+    """
+    def __init__(self):
+        pass
