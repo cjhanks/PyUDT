fca4e53d152605030b2ddd878b1c4fa8e5b5dd8f
Zeek Oni <web@cjhanks.name>
2012-12-11 10:38:51 -0800

    Add remaining intended functions

--- test_udt4.py
+++ test_udt4.py
@@ -5,6 +5,8 @@ import time
 import socket 
 import udt4 
 
+print('UDT_MTSS %i' % udt4.UDT_MSS)
+
 udt4.startup()
 print(socket.AF_INET)
 sock0 = udt4.socket(socket.AF_INET, socket.SOCK_STREAM, socket.AI_PASSIVE)
@@ -16,10 +18,7 @@ print(sock1)
 print(udt4.bind(sock0, '127.0.0.1', 4001))
 print('listen')
 udt4.listen(sock0, 40)
-udt4.accept(sock0)
 print('listen')
 
 udt4.cleanup() 
 
-
-time.sleep(100) 
